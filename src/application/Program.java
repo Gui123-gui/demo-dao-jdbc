14bd58287e244627b704e6fda6af28fe22ccc068
Guilherme Marini <gui_marini123@hotmail.com>
2025-01-04 15:35:31 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,8 +12,8 @@ public class Program {
 		Department obj = new Department(1, "books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+	
 		System.out.println(seller);
-
 	}
 
 }
